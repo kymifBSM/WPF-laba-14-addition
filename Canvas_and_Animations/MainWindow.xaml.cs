7b106d818f6e47d4978f8d9aff55f2a3bc104368
kymifBSM <sandmanfin@gmail.com>
2023-05-26 10:09:34 +0700

    complete

---
+++ Canvas_and_Animations/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Controls.Primitives;
+
+namespace Canvas_and_Animations
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        public lineControl Line;
+        public int Mode = 1;
+        
+        public int Delay = 0;
+        public int Speed = 2000;
+
+        private readonly List<lineControl> _lines = new List<lineControl>();
+        private System.Threading.Thread _animation;
+
+        private void Animate(object sender = null, RoutedEventArgs e = null)
+        {
+            
+            StopAnimation();
+            canvas.IsEnabled = false;
+
+            lineControl.CompleteAnimationCallback callback = delegate (lineControl l)
+            {
+                
+                Task.Run(delegate ()
+                {
+                    _animation = System.Threading.Thread.CurrentThread; //берём поток нащей функии чтобы потом в любой момент убить его
+                    foreach (lineControl line in _lines)
+                    {
+                        if (_animation == null) return;
+                        System.Threading.Thread.Sleep(Delay); //задержка
+                        App.Current.Dispatcher.Invoke(delegate ()
+                        {
+                            line.SetSpeed(new TimeSpan(0, 0, 0, 0, Speed)); //установка скорости
+                            line.BeginAnimation(); //старт анимации
+                        });
+
+                    }
+                });
+            };
+
+            _lines.Last().CompleteAnimationEvent += callback; //подписиваемся на событие
+
+            foreach (lineControl line in _lines)
+            {
+                line.line.X1 = line.line.X2 = line.line.Y1 = line.line.Y2 = 0; //скрываем линии
+            }
+            callback(null);
+
+        }
+
+        private void StopAnimation(object sender = null, RoutedEventArgs e = null)
+        {
+
+            _animation?.Abort(); //убиваем поток если он есть
+            _animation = null;
+
+            foreach (lineControl line in _lines) //убираем функции с события и останавливаем анимации
+            {
+                line.StopAnimation();
+                line.RemoveAllHandles_CompleteAnimationEvent();
+            }
+            canvas.IsEnabled = true;
+        }
+
+        private void SetLinePosition(int mode)
+        {
+            if (Line == null) return;
+            switch (mode)
+            {
+                case 1: //стандарт
+                    Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+                    Line.Y2 = Mouse.GetPosition(this).Y - canvas.Margin.Top;
+                    break;
+
+                case 2: //по углом 45 градусов слева
+                    Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+                    Line.Y2 = Line.Y1 - (Line.X1 - Mouse.GetPosition(this).X + canvas.Margin.Left);
+                    break;
+
+                case 3: //по углом 45 градусов справа
+                    Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+                    Line.Y2 = Line.Y1 + (Line.X1 - Mouse.GetPosition(this).X + canvas.Margin.Left);
+                    break;
+
+                case 4: //по вертикале
+                    Line.X2 = Line.X1;
+                    Line.Y2 = Mouse.GetPosition(this).Y - canvas.Margin.Top;
+                    break;
+
+                case 5: //по горизонтале
+                    Line.X2 = Mouse.GetPosition(this).X - canvas.Margin.Left;
+                    Line.Y2 = Line.Y1;
+                    break;
+            }
+            //Console.WriteLine($"X1: {l.X1}, X2: {l.X2}, Y1: {l.Y1}, Y2: {l.Y2}, V: {(l.X1 - Mouse.GetPosition(this).X)}"); //DEBUG
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            SetLinePosition(Mode); //обновляем линию
+        }
+
+        private void Canvas_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Line = new lineControl()
+            {
+                X1 = (Line == null) ? Mouse.GetPosition(this).X - canvas.Margin.Left : Line.X2,
+                Y1 = (Line == null) ? Mouse.GetPosition(this).Y - canvas.Margin.Top : Line.Y2,
+                X2 = Mouse.GetPosition(this).X - canvas.Margin.Left,
+                Y2 = Mouse.GetPosition(this).Y - canvas.Margin.Top
+            };
+            Line.line.StrokeThickness = 2; //шырина линии
+
+            _lines.Add(Line);
+            canvas.Children.Add(Line);
+            
+        }
+
+        private void Canvas_PreviewMouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+            SetLinePosition(Mode);
+            Line = null;
+
+        }
+
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.Source.GetType() == typeof(TextBox) || (!Keyboard.IsKeyDown(Key.LeftCtrl) && !Keyboard.IsKeyDown(Key.RightCtrl))) return; //проверка на Ctrl
+                Mode = Convert.ToInt32(e.Key.ToString()[1].ToString());
+                ModeChange_Click(Modes_grid.Children[Mode - 1]);
+            }
+            catch (Exception)
+            {
+
+            }
+            finally
+            {
+                
+                Canvas_MouseMove(null, null);
+            }
+
+            
+        }
+
+        
+
+        private void TB_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (sender.GetType() != typeof(TextBox)) return;
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+        private void Delay_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Delay = ((sender as TextBox).Text == "") ? 0 : Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void Speed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Speed = ((sender as TextBox).Text == "") ? 0 : Convert.ToInt32((sender as TextBox).Text);
+        }
+
+        private void ModeChange_Click(object sender, RoutedEventArgs e = null)
+        {
+            ToggleButton s = sender as ToggleButton;
+            UniformGrid parent = s.Parent as UniformGrid;
+
+            foreach (ToggleButton button in parent.Children)
+            {
+                button.IsChecked = false;
+            }
+            s.IsChecked = true;
+
+            Mode = Convert.ToInt32(s.Tag);
+            Canvas_MouseMove(null, null);
+        }
+    }
+}
---
+++ Canvas_and_Animations/lineControl.xaml.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+
+namespace Canvas_and_Animations
+{
+    /// <summary>
+    /// Interaction logic for lineControl.xaml
+    /// </summary>
+    public partial class lineControl : UserControl
+    {
+        public lineControl()
+        {
+            InitializeComponent();
+        }
+
+        static lineControl()
+        {
+            X1P = DependencyProperty.Register("X1", typeof(double), typeof(lineControl), new PropertyMetadata(0.1, X1_PC));
+            X2P = DependencyProperty.Register("X2", typeof(double), typeof(lineControl), new PropertyMetadata(0.1, X2_PC));
+            Y1P = DependencyProperty.Register("Y1", typeof(double), typeof(lineControl), new PropertyMetadata(0.1, Y1_PC));
+            Y2P = DependencyProperty.Register("Y2", typeof(double), typeof(lineControl), new PropertyMetadata(0.1, Y2_PC));
+        }
+
+
+        #region Property
+
+        public double X1
+        {
+            get { return (double)GetValue(X1P); }
+            set { SetValue(X1P, value); }
+        }
+
+        public double X2
+        {
+            get { return (double)GetValue(X2P); }
+            set { SetValue(X2P, value); }
+        }
+
+        public double Y1
+        {
+            get { return (double)GetValue(Y1P); }
+            set { SetValue(Y1P, value); }
+        }
+
+        public double Y2
+        {
+            get { return (double)GetValue(Y2P); }
+            set { SetValue(Y2P, value); }
+        }
+
+        #endregion
+
+        #region DependencyProperty
+
+        public static readonly DependencyProperty X1P;
+        public static readonly DependencyProperty X2P;
+        public static readonly DependencyProperty Y1P;
+        public static readonly DependencyProperty Y2P;
+
+        #endregion
+
+        #region Change calback functions
+
+        public static void X1_PC(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            lineControl c = obj as lineControl;
+            double nv = (double)e.NewValue;
+            c.X1 = c.line.X1 = nv;
+            c.ChangeAnimationValue();
+        }
+
+        public static void X2_PC(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            lineControl c = obj as lineControl;
+            double nv = (double)e.NewValue;
+            c.X2 = c.line.X2 = nv;
+            c.ChangeAnimationValue();
+        }
+
+        public static void Y1_PC(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            lineControl c = obj as lineControl;
+            double nv = (double)e.NewValue;
+            c.Y1 = c.line.Y1 = nv;
+            c.ChangeAnimationValue();
+        }
+
+        public static void Y2_PC(DependencyObject obj, DependencyPropertyChangedEventArgs e)
+        {
+            lineControl c = obj as lineControl;
+            double nv = (double)e.NewValue;
+            c.Y2 = c.line.Y2 = nv;
+            c.ChangeAnimationValue();
+        }
+
+        #endregion
+
+        #region Delegates structure
+
+        public delegate void CompleteAnimationCallback(lineControl lineControl);
+
+        #endregion
+
+        #region Class variables
+        //спикок функций(CompleteAnimationCallback) подписанных на событие CompleteAnimationEvent 
+        private readonly List<CompleteAnimationCallback> _listHandles = new List<CompleteAnimationCallback>();
+
+        public event CompleteAnimationCallback CompleteAnimationEvent
+        {
+            add
+            {
+                _listHandles.Add(value);
+            }
+
+            remove
+            {
+                _listHandles.Remove(value);
+            }
+        }
+        public TimeSpan SpeedAnimation { get; private set; }
+
+        #endregion
+
+        private void ChangeAnimationValue()
+        {
+            TimelineCollection collection = a.Storyboard.Children;//берём колекцию колекций кадров
+            //устанавливаем значения
+            (collection[0] as DoubleAnimationUsingKeyFrames).KeyFrames[0].Value = (collection[1] as DoubleAnimationUsingKeyFrames).KeyFrames[0].Value = (collection[0] as DoubleAnimationUsingKeyFrames).KeyFrames[1].Value = X1;
+            (collection[2] as DoubleAnimationUsingKeyFrames).KeyFrames[0].Value = (collection[3] as DoubleAnimationUsingKeyFrames).KeyFrames[0].Value = (collection[2] as DoubleAnimationUsingKeyFrames).KeyFrames[1].Value = Y1;
+
+            (collection[0] as DoubleAnimationUsingKeyFrames).KeyFrames[2].Value = X2;
+            (collection[1] as DoubleAnimationUsingKeyFrames).KeyFrames[1].Value = X2;
+            (collection[2] as DoubleAnimationUsingKeyFrames).KeyFrames[2].Value = Y2;
+            (collection[3] as DoubleAnimationUsingKeyFrames).KeyFrames[1].Value = Y2;
+        }
+
+        public void BeginAnimation()
+        {
+            a.Storyboard.Begin();
+        }
+
+        public void StopAnimation()
+        {
+            a.Storyboard.Stop();
+
+            //возвращаем значения
+            line.X1 = X1;
+            line.X2 = X2;
+            line.Y1 = Y1;
+            line.Y2 = Y2;
+        }
+
+        public void RemoveAllHandles_CompleteAnimationEvent()
+        {
+            for (int i = 0; i < _listHandles.Count; i++)
+            {
+                CompleteAnimationEvent -= _listHandles[i];
+                //_listHandles.Remove(_listHandles[i]);
+            }
+        }
+
+        public void SetSpeed(TimeSpan time)
+        {
+            StopAnimation();
+
+            TimelineCollection collection = a.Storyboard.Children;//берём колекцию колекций кадров
+            //устанавливаем значения
+            (collection[0] as DoubleAnimationUsingKeyFrames).KeyFrames[1].KeyTime = (collection[1] as DoubleAnimationUsingKeyFrames).KeyFrames[1].KeyTime = (collection[2] as DoubleAnimationUsingKeyFrames).KeyFrames[1].KeyTime = (collection[3] as DoubleAnimationUsingKeyFrames).KeyFrames[1].KeyTime = new TimeSpan(0, 0, 0, 0, (int)Math.Round(time.TotalMilliseconds / 2));
+            (collection[0] as DoubleAnimationUsingKeyFrames).KeyFrames[2].KeyTime = (collection[2] as DoubleAnimationUsingKeyFrames).KeyFrames[2].KeyTime = time;
+            SpeedAnimation = time;
+        }
+
+        private void CompleteAnimation(object sender, EventArgs e)
+        {
+            //CompleteAnimationEvent?.Invoke(this);
+            foreach (CompleteAnimationCallback callback in _listHandles)
+            {
+                callback(this);//вызываем функции
+            }
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            a.Storyboard.Completed += CompleteAnimation; //подписываемся на событие оканчании анимации
+        }
+    }
+}
